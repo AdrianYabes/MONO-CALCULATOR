e35668942ef30108b014931c3671d9cd23bbdd3b
Adrian Yabes <aidyabes13@gmail.com>
2021-07-01 14:11:27 +0800

    Encapsulation

--- MONO/Mono Calculator.cs
+++ MONO/Mono Calculator.cs
@@ -12,9 +12,9 @@ namespace MONO
 {
     public partial class Mono : Form
     {
-        String Mono_Operation = "";
-        String Num1 = "";
-        bool Mono_OperationPerformed = false;
+
+        OperatorClass Calculator = new OperatorClass();
+
 
         public Mono()
         {
@@ -24,10 +24,96 @@ namespace MONO
         {
 
         }
-
-        private void Operator_Click(object sender, EventArgs e)
+        private void One_Button_Click(object sender, EventArgs e)
         {
+            Append_Num("1");
+        }
+        private void Two_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("2");
+        }
+        private void Three_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("3");
+        }
+        private void Four_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("4");
+        }
+        private void Five_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("5");
+        }
+        private void Six_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("6");
+        }
+        private void Seven_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("7");
+        }
+        private void Eight_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("8");
+        }
+        private void Nine_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("9");
+        }
+        private void Zero_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num("0");
+        }
+        private void Point_Button_Click(object sender, EventArgs e)
+        {
+            Append_Num(".");
+        }
+        private void Operator_Addition(object sender, EventArgs e)
+        {
+            Calculator.Mono_Operation = "+";
+            Calculator.Mono_OperationPerformed = true;
+        }
+        private void Operator_Subtraction(object sender, EventArgs e)
+        {
+            Calculator.Mono_Operation = "-";
+            Calculator.Mono_OperationPerformed = true;
+        }
+        private void Operator_Multiplication(object sender, EventArgs e)
+        {
+            Calculator.Mono_Operation = "*";
+            Calculator.Mono_OperationPerformed = true;
+        }
+        private void Operator_Division(object sender, EventArgs e)
+        {
+            Calculator.Mono_Operation = "/";
+            Calculator.Mono_OperationPerformed = true;
+        }
+        private void Equal_Button_Click(object sender, EventArgs e)
+        {
+            Calculator.Num2 = Text_Output.Text;
+            if (Calculator.Mono_Operation.Equals("+"))
+                Calculator.Addition();
+            if (Calculator.Mono_Operation.Equals("-"))
+                Calculator.Subtraction();
+            if (Calculator.Mono_Operation.Equals("*"))
+                Calculator.Multiplication();
+            if (Calculator.Mono_Operation.Equals("/"))
+                Calculator.Division();
+            Text_Output.Text = Calculator.Num2;
+        }
+        private void Append_Num(string Number)
+        {
+            if (Calculator.Mono_OperationPerformed)
+            {
+                Calculator.Num1 = Text_Output.Text;
+                Text_Output.Text = "";
+            }
+            Text_Output.Text += Number;
+            Calculator.Mono_OperationPerformed = false;
+        }
 
+        private void Text_Output_TextChanged(object sender, EventArgs e)
+        {
         }
         private void Clear_Button_Click(object sender, EventArgs e)
         {
@@ -37,134 +123,5 @@ namespace MONO
         {
             Text_Output.Text = "";
         }
-
-        private void One_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("1");
-
-        }
-
-        private void Two_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("2");
-
-        }
-
-        private void Three_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("3");
-
-        }
-
-        private void Four_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("4");
-
-        }
-
-        private void Five_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("5");
-
-        }
-
-        private void Six_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("6");
-
-        }
-
-        private void Seven_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("7");
-
-        }
-
-        private void Eight_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("8");
-
-        }
-
-        private void Nine_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("9");
-
-        }
-
-        private void Zero_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num("0");
-
-        }
-
-        private void Point_Button_Click(object sender, EventArgs e)
-        {
-            Append_Num(".");
-        }
-        private void Operator_Addition(object sender, EventArgs e)
-        {
-            Mono_Operation = "+";
-            Mono_OperationPerformed = true;
-        }
-        private void Operator_Subtraction(object sender, EventArgs e)
-        {
-            Mono_Operation = "-";
-            Mono_OperationPerformed = true;
-        }
-        private void Operator_Multiplication(object sender, EventArgs e)
-        {
-            Mono_Operation = "*";
-            Mono_OperationPerformed = true;
-        }
-        private void Operator_Division(object sender, EventArgs e)
-        {
-            Mono_Operation = "/";
-            Mono_OperationPerformed = true;
-        }
-        private void Equal_Button_Click(object sender, EventArgs e)
-        {
-            if (Mono_Operation.Equals("+"))
-                Addition();
-            if (Mono_Operation.Equals("-"))
-                Subtraction();
-            if (Mono_Operation.Equals("*"))
-                Multiplication();
-            if (Mono_Operation.Equals("/"))
-                Division();
-        }
-        private void Addition()
-        {
-            Text_Output.Text = (Double.Parse(Num1) + Double.Parse(Text_Output.Text)).ToString();
-        }
-        private void Subtraction()
-        {
-            Text_Output.Text = (Double.Parse(Num1) - Double.Parse(Text_Output.Text)).ToString();
-        }
-        private void Multiplication()
-        {
-            Text_Output.Text = (Double.Parse(Num1) * Double.Parse(Text_Output.Text)).ToString();
-        }
-        private void Division()
-        {
-            Text_Output.Text = (Double.Parse(Num1) / Double.Parse(Text_Output.Text)).ToString();
-        }
-
-
-        private void Append_Num(string Number)
-        {
-            if (Mono_OperationPerformed)
-            {
-                Num1 = Text_Output.Text;
-                Text_Output.Text = "";
-            }
-            Text_Output.Text += Number;
-            Mono_OperationPerformed = false;
-        }
-
-        private void Text_Output_TextChanged(object sender, EventArgs e)
-        {
-        }
-
     }
 }
---
+++ MONO/OperatorClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONO
+{
+    class OperatorClass
+    {
+        private String mono_Operation = "";
+        private String num1 = "";
+        private String num2 = "";
+        private bool mono_OperationPerformed = false;
+
+        public string Mono_Operation { get => mono_Operation; set => mono_Operation = value; }
+        public string Num1 { get => num1; set => num1 = value; }
+        public bool Mono_OperationPerformed { get => mono_OperationPerformed; set => mono_OperationPerformed = value; }
+        public string Num2 { get => num2; set => num2 = value; }
+
+        public void Addition()
+        {
+            Num2 = (Double.Parse(Num1) + Double.Parse(Num2)).ToString();
+        }
+        public void Subtraction()
+        {
+            Num2 = (Double.Parse(Num1) - Double.Parse(Num2)).ToString();
+        }
+        public void Multiplication()
+        {
+            Num2 = (Double.Parse(Num1) * Double.Parse(Num2)).ToString();
+        }
+        public void Division()
+        {
+            Num2 = (Double.Parse(Num1) / Double.Parse(Num2)).ToString();
+        }
+    }
+}
